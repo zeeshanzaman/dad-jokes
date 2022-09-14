cacaee6e1339ad91d0f79d2835c6e7ef36574834
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-14 12:03:00 +0500

    create generate Joke function

--- custom.js
+++ custom.js
@@ -1,1 +1,26 @@
-const jokeEl = document.getElementById('joke')
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+// on click it will give us a new joke 
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => {
+            jokeEl.innerHTML = data.joke
+        })
+}
+
+// first in function generateJoke() for understanding it we use 
+// fech('https://icanhazdadjoke.com', config)
+// res is we for (request)
+// .then((res) => res.json())
+// .then((data) => console.log(data))
